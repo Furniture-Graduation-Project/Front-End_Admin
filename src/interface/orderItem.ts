70e2510d4273f51b564f551ae39b33268c5b79ef
PhamHaiHoan <150771122+PhamHaiHoan@users.noreply.github.com>
2024-11-22 00:58:51 +0700

    Sửa nút Button Oder và làm OderDetail

--- src/interface/orderItem.ts
+++ src/interface/orderItem.ts
@@ -1,4 +1,8 @@
 export interface IOrderItem {
+  variants: any
+  price: any
+  productName: ReactNode
+  _id: Key | null | undefined
   productId: string
   productOptionId: string
   unitPrice: number
