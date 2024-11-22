b8349f801d85693e3f39632afac7ceb17b1137dc
PhamHaiHoan <150771122+PhamHaiHoan@users.noreply.github.com>
2024-11-22 09:18:31 +0700

    xóa dữ liệu thừa interface

--- src/interface/orderItem.ts
+++ src/interface/orderItem.ts
@@ -1,8 +1,4 @@
 export interface IOrderItem {
-  variants: any
-  price: any
-  productName: ReactNode
-  _id: Key | null | undefined
   productId: string
   productOptionId: string
   unitPrice: number
