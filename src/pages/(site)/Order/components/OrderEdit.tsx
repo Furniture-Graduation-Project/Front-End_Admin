c50414c4fea3a111fcc3ba4e9b61c12de9cccd00
Your Name <junnguyenvizito@gmail.com>
2024-12-09 18:27:04 +0700

    Check price and quantity product in return order

--- src/pages/(site)/Order/components/OrderEdit.tsx
+++ src/pages/(site)/Order/components/OrderEdit.tsx
@@ -90,43 +90,43 @@ const OrderEdit = () => {
   const hanleChangePayment = async () => {
     if (
       orderData?.data?.data._id &&
-      orderData?.data?.data.status !== 'received' &&
-      orderData?.data?.data.payment?.paymentStatus !== 'paid'
+      orderData?.data?.data.status == 'received' &&
+      orderData?.data?.data.payment?.paymentStatus == 'paid'
     ) {
-      if (
-        orderData?.data?.data.status == 'unpaid' &&
-        paymentStatus == 'paid' &&
-        orderData?.data?.data.payment?.paymentStatus == 'unpaid'
-      ) {
-        const newStatus = {
-          _id: orderData?.data?.data._id,
-          status: 'pending',
-          payment: {
-            ...orderData?.data?.data.payment,
-            paymentStatus: 'paid'
-          }
-        }
-        mutate(newStatus)
-        setIsModalOpen(false)
-        return
-      } else {
-        const newStatus = {
-          _id: orderData?.data?.data._id,
-          payment: {
-            ...orderData?.data?.data.payment,
-            paymentStatus: paymentStatus
-          }
-        }
-        mutate(newStatus)
-        setIsModalOpen(false)
-      }
-    } else {
       setIsModalOpen(false)
       toast({
         title: 'Cập nhật thất bại',
         description: 'Không thể thay đổi trạng thái thanh toán đơn hàng đã hoàn thành !',
         variant: 'destructive'
       })
+      return
+    }
+    if (
+      orderData?.data?.data.status == 'unpaid' &&
+      paymentStatus == 'paid' &&
+      orderData?.data?.data.payment?.paymentStatus == 'unpaid'
+    ) {
+      const newStatus = {
+        _id: orderData?.data?.data._id,
+        status: 'pending',
+        payment: {
+          ...orderData?.data?.data.payment,
+          paymentStatus: 'paid'
+        }
+      }
+      mutate(newStatus)
+      setIsModalOpen(false)
+      return
+    } else {
+      const newStatus = {
+        _id: orderData?.data?.data._id,
+        payment: {
+          ...orderData?.data?.data.payment,
+          paymentStatus: paymentStatus
+        }
+      }
+      mutate(newStatus)
+      setIsModalOpen(false)
     }
   }
   useEffect(() => {
