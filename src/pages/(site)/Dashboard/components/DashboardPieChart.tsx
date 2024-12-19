cec092c8b49363305e4a1973b2d1b4bb7e6684ee
PhamHaiHoan <150771122+PhamHaiHoan@users.noreply.github.com>
2024-12-19 15:27:00 +0700

    Sửa text ở biểu đò

--- src/pages/(site)/Dashboard/components/DashboardPieChart.tsx
+++ src/pages/(site)/Dashboard/components/DashboardPieChart.tsx
@@ -50,8 +50,8 @@ const DashboardPieChart = () => {
   return (
     <Card className='dark:bg-'>
       <CardHeader>
-        <CardTitle className='text-black'>Biểu đồ cột thống kê danh mục</CardTitle>
-        <CardDescription>Hiển thị tổng số lượt truy cập trong 6 tháng qua</CardDescription>
+        <CardTitle className='text-black'>Biểu đồ cột thống kê đơn hàng</CardTitle>
+        <CardDescription>Hiển thị tổng số đơn hàng trong 6 tháng qua</CardDescription>
       </CardHeader>
       <CardContent style={{ height: '500px' }}>
         <ResponsiveContainer width='100%' height='100%'>
